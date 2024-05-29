736c839c77fd49964eb5f52fe0059d1faae4311a
Julian Olalde <julianolaldeabarca@icloud.com>
2024-05-28 23:22:14 -0600

    Ya ahora si

--- Controlador.php
+++ Controlador.php
@@ -3,7 +3,7 @@
     $Servidor = "127.0.0.1";
     $Usuario = "root";
     $Password = "";
-    $BD = "controvehicularfinal";
+    $BD = "controlvehicularfinal";
     $Con = mysqli_connect($Servidor,$Usuario,$Password,$BD);
     return $Con;
   }
