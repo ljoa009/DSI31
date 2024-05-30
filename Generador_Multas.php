457dd7b1257338e095a185194b878ba08dc41b84
Julian Olalde <julianolaldeabarca@icloud.com>
2024-05-29 18:06:00 -0600

    Imagen y Multas 70%

--- Generador_Multas.php
+++ Generador_Multas.php
@@ -5,6 +5,7 @@
        
           require('fpdf.php');
           require('Controlador.php');
+          
 
           $conexion = Conectar();
 
@@ -12,28 +13,35 @@
           $sql = "SELECT * FROM vistamulta WHERE MultaId=3";
           $resultset = Ejecutar($conexion, $sql);
 
-          $pdf = new FPDF('P', 'mm', array(216,330));
+          $pdf = new FPDF('P', 'mm', array(216,350));
           $pdf->AddPage();
           $pdf->SetFont('Arial', '', 8);
           $pdf->SetAutoPageBreak(true, 10);
-          //$pdf->Image('Imagenes/FondoMulta.png',0,0,210,297);
+          $pdf->Image('Imagenes/Foto_Multa.jpeg',10,0,40,40);
 
           // Establecer el encabezado fuera del recuadro
-          $pdf->SetFont('Arial','B',12);
-          $pdf->Cell(0, 10, 'SECRETARIA DE SEGURIDAD CIUDADANA', 0, 1, 'C');
-          $pdf->SetFont('Arial','',10);
-          $pdf->Cell(0, 10, 'SUBSECRETARIA DE POLICIA ESTATAL', 0, 1, 'C');
-          $pdf->Ln(20);
+          $pdf->SetFont('Arial','B',20);
+          $pdf->SetXY(50,15);
+          $pdf->Cell(0, 10, 'SECRETARIA DE SEGURIDAD CIUDADANA', 0, 1, 'L');
+          $pdf->SetFont('Arial','',20);
+          $pdf->SetX(50);
+          $pdf->Cell(0, 5, 'SUBSECRETARIA DE POLICIA ESTATAL', 0, 1, 'L');
+          $pdf->Ln(15);
+
+          $pdf->SetFont('Arial','',9); // Ajusta el tamaño de la fuente según sea necesario
+          $pdf->SetX(10); // Ajusta el margen izquierdo según sea necesario
+          $pdf->MultiCell(0, 5, 'Con fundamento en los articulos 31 fraccion II de la Ley de Transito para el Estado de Queretaro y 6 fraccion II, inciso g) del Reglamento de la Ley de Transito para el Estado de Queretaro, se emite la presente boleta de infraccion: ', 0, 'L');
+          $pdf->Ln(10);
 
           // Iterar sobre los resultados y generar las multas
           if ($resultset->num_rows > 0) {
               while($row = $resultset->fetch_assoc()) {
+
                   // Guardar la posición actual del Y
                   $startY = $pdf->GetY();
                   
                   // Dibujar el rectángulo para los datos
                   $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
-                  
                   // Establecer los datos de la multa dentro del rectángulo
                   $pdf->SetFont('Arial','',10);
                   $pdf->SetXY(15, $startY + 5); 
@@ -43,13 +51,12 @@
                   $pdf->Cell(0, 7, 'Hora: ' . $row['Hora'], 0, 1);
                   $pdf->SetX(15); 
                   $pdf->Cell(0, 7, 'No. de Multa: ' . $row['Folio'], 0, 1);
-
                   // Ajustar la posición Y para evitar superposición con el siguiente elemento
                   $startY += 40;
 
                   // Dibujar el rectángulo para los datos
-                  $pdf->Rect(10, $startY, 200, 30); // Coord. X, Coord. Y, Ancho, Alto
-                  // Establecer los datos de la multa dentro del rectángulo
+                  $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
+                 // Establecer los datos de la multa dentro del rectángulo
                   $pdf->SetFont('Arial','',10);
                   $pdf->SetXY(15, $startY + 5); 
                   // Posiciona el cursor para el texto dentro del rectángulo
@@ -59,28 +66,60 @@
                   $pdf->SetX(15); 
                   $pdf->Cell(0, 7, 'Domicilio: ' . $row['ConductorCalle'], 0, 1);
                   // Ajustar la posición Y para evitar superposición con el siguiente elemento
-                  $pdf->SetY($startY + 30);
+                  $startY += 40;
 
-                  
-                  $pdf->Cell(0, 10, 'Vehículo: ' . $row['VehiculoMarca'] . ' ' . $row['VehiculoModelo'], 0, 1);
-                  $pdf->Cell(0, 10, 'Año: ' . $row['VehiculoAño'], 0, 1);
-                  $pdf->Cell(0, 10, 'Placas: ' . $row['VehiculoPlaca'], 0, 1);
-                  $pdf->Ln(10);
-                  
-                  $pdf->Cell(0, 10, 'Motivo de la infracción: ' . $row['Motivo'], 0, 1);
-                  $pdf->Ln(10);
-                  
-                  $pdf->Cell(0, 10, 'Observaciones del Operativo: ' . $row['ObsPersonal'], 0, 1);
-                  $pdf->Ln(10);
-                  
-                  $pdf->Cell(0, 10, 'Agente: ' . $row['OficialNombre'] . ' ' . $row['OficialApellido'], 0, 1);
-                  $pdf->Cell(0, 10, 'Grupo, Region o Asignacion: ' . $row['OficialGRA'], 0, 1);
-                  $pdf->Cell(0, 10, 'No. de Agente: ' . $row['OficialId'], 0, 1);
-                  $pdf->Ln(10);
-                  
+                  // Dibujar el rectángulo para los datos
+                  $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
+                 // Establecer los datos de la multa dentro del rectángulo
+                  $pdf->SetFont('Arial','',10);
+                  $pdf->SetXY(15, $startY + 5); 
+                  // Posiciona el cursor para el texto dentro del rectángulo
+                  $pdf->Cell(0, 7, 'Vehiculo: ' . $row['VehiculoMarca'] . ' ' . $row['VehiculoModelo'], 0, 1);
+                  $pdf->SetX(15); 
+                  $pdf->Cell(0, 7, 'Anio: ' . $row['VehiculoAño'], 0, 1);
+                  $pdf->SetX(15); 
+                  $pdf->Cell(0, 7, 'Placas: ' . $row['VehiculoPlaca'], 0, 1);
+                  // Ajustar la posición Y para evitar superposición con el siguiente elemento
+                  $startY += 40;
+
+                   // Dibujar el rectángulo para los datos
+                  $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
+                 // Establecer los datos de la multa dentro del rectángulo
+                  $pdf->SetFont('Arial','',10);
+                  $pdf->SetXY(15, $startY + 5); 
+                  // Posiciona el cursor para el texto dentro del rectángulo
+                  $pdf->Cell(0, 7, 'Motivo de la infraccion: ' . $row['Motivo'], 0, 1);
+                  $pdf->SetX(15); 
+                  $pdf->Cell(0, 7, 'Observaciones del Operativo: ' . $row['ObsPersonal'], 0, 1);
+                  // Ajustar la posición Y para evitar superposición con el siguiente elemento
+                  $startY += 40;
+
+                  // Dibujar el rectángulo para los datos
+                  $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
+                 // Establecer los datos de la multa dentro del rectángulo
+                  $pdf->SetFont('Arial','',10);
+                  $pdf->SetXY(15, $startY + 5); 
+                  // Posiciona el cursor para el texto dentro del rectángulo
+                  $pdf->Cell(0, 7, 'Agente: ' . $row['OficialNombre'] . ' ' . $row['OficialApellido'], 0, 1);
+                  $pdf->SetX(15); 
+                  $pdf->Cell(0, 7, 'Grupo, Region o Asignacion: ' . $row['OficialGRA'], 0, 1);
+                  $pdf->SetX(15); 
+                  $pdf->Cell(0, 7, 'No. de Agente: ' . $row['OficialId'], 0, 1);
+                  // Ajustar la posición Y para evitar superposición con el siguiente elemento
+                  $startY += 40;
+
+                   // Dibujar el rectángulo para los datos
+                  $pdf->Rect(10, $startY, 190, 30); // Coord. X, Coord. Y, Ancho, Alto
+                 // Establecer los datos de la multa dentro del rectángulo
+                  $pdf->SetFont('Arial','',10);
+                  $pdf->SetXY(15, $startY + 5); 
+                  // Posiciona el cursor para el texto dentro del rectángulo
                   $pdf->Cell(0, 10, 'Clasificación de la boleta: ' . $row['ClasBoleta'], 0, 1);
+                  $pdf->SetX(15); 
                   $pdf->Cell(0, 10, 'Observaciones del Conductor: ' . $row['ObsConductor'], 0, 1);
-                  $pdf->Ln(20);
+                  // Ajustar la posición Y para evitar superposición con el siguiente elemento
+                  $startY += 40;
+                   
               }
           } else {
               $pdf->Cell(0, 10, 'No se encontraron resultados', 0, 1);
